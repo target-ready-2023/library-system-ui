ba693e841969dbe838ddeabefd851cb50ab0d82b
ISHAASTHANA <ish.asthana@gmail.com>
2023-07-19 23:19:00 +0530

    addition of snackbar

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,10 @@
+import { Card } from "@mui/material"
+
+const Category = () => {
+    return (
+        <Card className="App-Card">
+            <h3>Category</h3>
+        </Card>
+    )
+}
+export default Category
