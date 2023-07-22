f1e5804883f34e0e1fc53b80e40c58673d7d3d85
Jahnavi8122 <jahnavipabbathi812@gmail.com>
2023-07-22 20:41:30 +0530

    Added Pagenation

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,11 @@
 import Logo from "../Logo";
 import SideNavigation from "../SideNavigation";
+import './header.css';
 
 const Header = () => {
   return (
     <>
-      <header className="App-header">
+      <header  className="App-header">
         <Logo />
         <h2>Digital Library</h2>
         <SideNavigation />
