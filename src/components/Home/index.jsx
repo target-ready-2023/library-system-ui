00ddc5827b3781512cfdee54286bbbd82ca524a9
Jahnavi8122 <jahnavipabbathi812@gmail.com>
2023-07-30 20:56:26 +0530

    changed the front end design to tabular format

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -12,6 +12,9 @@ import axios from 'axios';
 import { ChevronLeft, ChevronRight } from '@mui/icons-material';
 import { useLocation } from 'react-router-dom';
 import { red } from "@mui/material/colors";
+import { Delete, Edit } from '@mui/icons-material';
+import { AddCircleOutline, RemoveCircleOutline } from '@mui/icons-material';
+
 
 const Home = () => {
   const [data, setData] = useState([]);
@@ -28,6 +31,35 @@ const [currentPage,setCurrentPage]=useState(0);
   const firstIndex=lastIndex-recordsPerPage;
   const records=data.slice(firstIndex,lastIndex);
   const nPage=Math.ceil(data.length/recordsPerPage);
+
+  const columnStyles = {
+    backgroundColor: 'lightBlue',
+    width: '90%', 
+    height: '70px',
+    margin: '5px', 
+    left:'10px',
+    textAlign: 'left',
+    fontFamily: 'TimesNewRoman',
+    fontSize: '20px',
+    padding: '10px',
+    borderRadius: '5px',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+    transition: 'transform 0.3s',
+    '&:hover': {
+      transform: 'scale(1.01)',
+    },
+  };
+
+    const [count, setCount] = useState(0);
+  
+    const handleIncrement = () => {
+      setCount((prevCount) => prevCount + 1);
+    };
+  
+    const handleDecrement = () => {
+      setCount((prevCount) => prevCount - 1);
+    };
+  
    
   const toggleDrawer = () => {
     setDrawerOpen(!drawerOpen);
@@ -99,76 +131,83 @@ const [currentPage,setCurrentPage]=useState(0);
 
 
     return (
-        <div sx={{paddingTop:'70px', fontFamily:'TimesNewRoman'}}>
+        <div sx={{paddingTop:'70px', fontFamily:'TimesNewRoman',}}>
         <Card className="App-Card">
             <h3>Books Directory</h3>
             <div>
-                {data.map((item) => (
-                    <div >
-                      
-                    <p key={item.book_id}>
-                     <Button onClick={() => handleToggleDrawer(item)}  
-                     sx={{ width: '100%',
-                     height: '70px',
-                     marginTop:'10px',
-                     textAlign: 'left',
-                     fontFamily:'TimesNewRoman',
-                     fontSize:'20px',
-                     padding:'10px',
-                     color:'black'}}>
-                     <Box sx={{
-                        backgroundColor: 'lightBlue',
-                        width: '84%',
-                        height: '70px',
-                        marginLeft:'210px',
-                        marginTop:'5px',
-                        textAlign: 'left',
-                        fontFamily:'TimesNewRoman',
-                        fontSize:'20px',
-                        padding:'10px',
-                        borderRadius: '5px',
-                        boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
-                        transition: 'transform 0.3s', 
-                        '&:hover': {
-                          transform: 'scale(1.01)', 
-                        },
-           
-        }}>
-                        {item.book_name}
+              <table style={{ width: '80%', borderCollapse: 'collapse',marginLeft:'200px' }}>
+                <thead>
+                  <tr>
+                    <th>Book Name</th>
+                    <th>Book Description</th>
+                    {/* <th>Category Name</th> */}
+                    <th>Author Name</th>
+                    <th>Publication Year</th>
+                    <th>Issue Book</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                  </tr>
+                </thead>
+                  <tbody>
+                  {data.map((item) => (
+                    <tr key={item.book_id}>
+                      <td>
+                        <Box sx={columnStyles}>
+                          {item.book_name}
                         </Box>
-                     </Button>
-                    
-                     </p>
-                      
-                    </div> 
-                    ))}
-                    <Drawer
-                    anchor="right"
-                    open={drawerOpen}
-                    onClose={() => setDrawerOpen(false)}
-                    PaperProps={{ style: { height: '100%', width:'70%', alignItems: 'center', justifyContent: 'center'} }}
-                  >
-                    {selectedBook && (
-                    <div className="details">
-                          <Button style={{Color:red,marginLeft:'85%'}} onClick={() => deleteBook(selectedBook.book_id)}>Delete Book</Button>
-                         <h2>{selectedBook.book_name.toUpperCase()}</h2>
-                        <Box sx={{fontSize:"25px",fontFamily:'TimesNewRoman'}}>
-                        <p>{selectedBook.book_description}</p>
-                        <p>{selectedBook.category_name}</p>
-                        <p>{selectedBook.author_name}</p>
-                        <p>{selectedBook.publication_year}</p>
+                      </td>
+                      <td >
+                        <Box sx={columnStyles}>
+                          {item.book_description}
                         </Box>
-                    </div>
-                    )}
-                  
-                  </Drawer>
-                                
-            </div>
+                      </td>
+                      <td>
+                        <Box sx={columnStyles}>
+                          {item.author_name}
+                        </Box>
+                      </td>
+                      <td >
+                        <Box sx={columnStyles}>
+                          {item.publication_year}
+                        </Box>
+                      </td>
+                      <td >
+                        <Box sx={columnStyles}>
+                        
+                          <button onClick={handleDecrement}>
+                            <RemoveCircleOutline />
+                          </button>
+                          <span>{count}</span>
+                          <button onClick={handleIncrement}>
+                            <AddCircleOutline />
+                          </button>
+                          
+                        
+                        </Box>
+                      </td>
+                      <td >
+                      <button className="update-button" >
+                          <i className="fas fa-trash"></i> 
+                          <Edit />
+                          Update
+                        </button>
+                      </td>
+                      <td >
+                        <button className="delete-button" onClick={() => deleteBook(item.book_id)}>
+                          <i className="fas fa-trash"></i> 
+                          <Delete />
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
 
+              </table>
+            </div>
             <ul className="pagination">
               <li className="page-item">
                 <a href="#" className="page-link" onClick={() => { if (currentPage > 0) changePage(currentPage - 1) }}>
-
                     previous
                 </a>
               </li>
