b9edc6e991287ffc1be5ba31c0a270a3e71d5b97
Pavan Kalyan T <tpavankalyan18@gmail.com>
2023-08-04 03:45:34 +0530

    Frontend changes- form, icons, display, categories

--- src/components/UpdateButton/index.jsx
+++ src/components/UpdateButton/index.jsx
@@ -1,45 +1,80 @@
+// import React, { useState } from "react";
+// import { Button, Drawer, Box } from "@mui/material";
+// import { Edit } from "@mui/icons-material";
+// import UpdateForm from "../UpdateForm";
+
+// const UpdateButton = ({ item, handleToggleDrawer }) => {
+//   const [drawerOpen, setDrawerOpen] = useState(false);
+//   const [selectedBook, setSelectedBook] = useState(null);
+
+//   const handleOpenDrawer = () => {
+//     setSelectedBook(item);
+//     setDrawerOpen(!drawerOpen);
+//   };
+
+//   const handleCloseDrawer = () => {
+//     setDrawerOpen(false);
+//   };
+
+//   return (
+//     <div>
+//       <Button className="update-button" onClick={handleOpenDrawer}>
+//         <Edit />
+//       </Button>
+//       <Drawer
+//         anchor="top"
+//         open={drawerOpen}
+//         onClose={handleCloseDrawer}
+//         PaperProps={{
+//           style: {
+//             height: "80%",
+//             alignItems: "center",
+//             justifyContent: "center",
+//           },
+//         }}
+//       >
+//         <Box p={2} style={{ border: "black" }}>
+//           <UpdateForm
+//             selectedBook={selectedBook}
+//             handleCloseDrawer={handleCloseDrawer}
+//           />
+//         </Box>
+//       </Drawer>
+//     </div>
+//   );
+// };
+
+// export default UpdateButton;
+
 import React, { useState } from "react";
-import { Button, Drawer, Box } from "@mui/material";
+import { Button } from "@mui/material";
 import { Edit } from "@mui/icons-material";
 import UpdateForm from "../UpdateForm";
 
-const UpdateButton = ({ item, handleToggleDrawer }) => {
-  const [drawerOpen, setDrawerOpen] = useState(false);
+const UpdateButton = ({ item }) => {
+  const [isUpdateFormOpen, setIsUpdateFormOpen] = useState(false);
   const [selectedBook, setSelectedBook] = useState(null);
 
-  const handleOpenDrawer = () => {
+  const handleOpenUpdateForm = () => {
     setSelectedBook(item);
-    setDrawerOpen(!drawerOpen);
+    setIsUpdateFormOpen(true);
   };
 
-  const handleCloseDrawer = () => {
-    setDrawerOpen(false);
+  const handleCloseUpdateForm = () => {
+    setIsUpdateFormOpen(false);
   };
 
   return (
     <div>
-      <Button className="update-button" onClick={handleOpenDrawer}>
-        <Edit />
+      <Button className="update-button" onClick={handleOpenUpdateForm}>
+        <Edit style={{ fontSize: 25, padding: 1 }} />
       </Button>
-      <Drawer
-        anchor="top"
-        open={drawerOpen}
-        onClose={handleCloseDrawer}
-        PaperProps={{
-          style: {
-            height: "80%",
-            alignItems: "center",
-            justifyContent: "center",
-          },
-        }}
-      >
-        <Box p={2} style={{ border: "black" }}>
-          <UpdateForm
-            selectedBook={selectedBook}
-            handleCloseDrawer={handleCloseDrawer}
-          />
-        </Box>
-      </Drawer>
+      {isUpdateFormOpen && (
+        <UpdateForm
+          selectedBook={selectedBook}
+          handleCloseForm={handleCloseUpdateForm}
+        />
+      )}
     </div>
   );
 };
--- src/components/UpdateForm/index.jsx
+++ src/components/UpdateForm/index.jsx
@@ -1,48 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { TextField, Button, FormLabel } from "@mui/material";
-import { Box } from "@mui/material";
+import {
+  Snackbar,
+  Alert,
+  Chip,
+  TextField,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+} from "@mui/material";
 import axios from "axios";
+import "./UpdateForm.css";
 
-// const UpdateForm = ({ selectedBook, handleCloseDrawer }) => {
-//   const resetForm = () => {
-//     setBook({
-//       bookName: "",
-//       bookDescription: "",
-//       publicationYear: 0,
-//       authorName: "",
-//     });
-//     setCategoryNames([]);
-//     setNewCategory("");
-//   };
-//   //   const [book, setBook] = useState(
-//   //     selectedBook || {
-//   //       bookName: "",
-//   //       bookDescription: "",
-//   //       publicationYear: 0,
-//   //       authorName: "",
-//   //     }
-//   //   );
-//   const [book, setBook] = useState({
-//     bookName: selectedBook?.bookName || "",
-//     bookDescription: selectedBook?.bookDescription || "",
-//     publicationYear: selectedBook?.publicationYear || "",
-//     authorName: selectedBook?.authorName || "",
-//   });
-
-//   useEffect(() => {
-//     setBook(
-//       selectedBook || {
-//         bookName: "",
-//         bookDescription: "",
-//         publicationYear: 0,
-//         authorName: "",
-//       }
-//     );
-//   }, [selectedBook]);
-
-//   const [categoryNames, setCategoryNames] = useState([]);
-//   const [newCategory, setNewCategory] = useState("");
-const UpdateForm = ({ selectedBook, handleCloseDrawer }) => {
+const UpdateForm = ({ selectedBook, handleCloseForm }) => {
+  // setOpen(true);
   const resetForm = () => {
     setBook({
       bookName: "",
@@ -74,6 +46,16 @@ const UpdateForm = ({ selectedBook, handleCloseDrawer }) => {
 
   const [categoryNames, setCategoryNames] = useState([]);
   const [newCategory, setNewCategory] = useState("");
+  const [open, setOpen] = useState(true);
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+
+  const handleClose = () => {
+    setOpen(false);
+    resetForm();
+    handleCloseForm();
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -94,6 +76,12 @@ const UpdateForm = ({ selectedBook, handleCloseDrawer }) => {
     ]);
     setNewCategory("");
   };
+  const handleCloseSnackbar = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setOpenSnackbar(false);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -112,97 +100,130 @@ const UpdateForm = ({ selectedBook, handleCloseDrawer }) => {
       .put(updateApiUrl, data)
       .then((response) => {
         console.log("Book updated successfully.");
-        handleCloseDrawer(); // Close the drawer after successful update
+        setSnackbarMessage("Book updated successfully!");
+        setSnackbarSeverity("success");
+        setOpenSnackbar(true);
+        handleClose();
       })
       .catch((error) => {
         console.error("Error updating the book:", error.message);
       });
-    resetForm();
   };
 
   return (
-    <div>
-      <FormLabel
-        sx={{
-          marginBottom: "10px",
-          fontFamily: "Times New Roman",
-          fontSize: "50px",
-        }}
+    <Dialog
+      open={open}
+      maxWidth="md"
+      fullWidth
+      classes={{ paper: "dialogContainer" }}
+    >
+      <DialogTitle>Update Book Details</DialogTitle>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            name="bookName"
+            label="Book Name"
+            value={book.bookName}
+            onChange={handleChange}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            name="bookDescription"
+            label="Book Description"
+            value={book.bookDescription}
+            onChange={handleChange}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            name="publicationYear"
+            label="Publication Year"
+            value={book.publicationYear}
+            onChange={handleChange}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            name="authorName"
+            label="Author Name"
+            value={book.authorName}
+            onChange={handleChange}
+            fullWidth
+            required
+            style={{ marginBottom: "10px" }}
+          />
+          <TextField
+            label="Categories"
+            value={newCategory}
+            onChange={handleCategoryChange}
+            InputProps={{
+              endAdornment: (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={addCategory}
+                  disabled={!newCategory.trim()}
+                >
+                  Add
+                </Button>
+              ),
+            }}
+            fullWidth
+            style={{ marginBottom: "10px" }}
+          />
+          {/* <div>
+            {categoryNames.map((category, index) => (
+              <div key={index}>{category}</div>
+            ))}
+          </div> */}
+          <div>
+            {categoryNames.map((category, index) => (
+              <Chip
+                key={index}
+                label={category}
+                onDelete={() =>
+                  setCategoryNames((prevCategories) =>
+                    prevCategories.filter((_, i) => i !== index)
+                  )
+                }
+                style={{ margin: 4 }}
+              />
+            ))}
+          </div>
+          <div className="dialogActionsButtonGroup">
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: "10px" }}
+            >
+              Update
+            </Button>
+            <Button onClick={handleClose} variant="contained" color="primary">
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </DialogContent>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
       >
-        Update Book Details
-      </FormLabel>
-
-      <form onSubmit={handleSubmit}>
-        <TextField
-          name="bookName"
-          label="Book Name"
-          value={book.bookName}
-          onChange={handleChange}
-          fullWidth
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          name="bookDescription"
-          label="Book Description"
-          value={book.bookDescription}
-          onChange={handleChange}
-          fullWidth
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          name="publicationYear"
-          label="Publication Year"
-          value={book.publicationYear}
-          onChange={handleChange}
-          fullWidth
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          name="authorName"
-          label="Author Name"
-          value={book.authorName}
-          onChange={handleChange}
-          fullWidth
-          required
-          style={{ marginBottom: "10px" }}
-        />
-        <TextField
-          label="Categories"
-          value={newCategory}
-          onChange={handleCategoryChange}
-          InputProps={{
-            endAdornment: (
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={addCategory}
-                disabled={!newCategory.trim()}
-              >
-                Add
-              </Button>
-            ),
-          }}
-          fullWidth
-          style={{ marginBottom: "10px" }}
-        />
-        <div>
-          {categoryNames.map((category, index) => (
-            <div key={index}>{category}</div>
-          ))}
-        </div>
-        <Button
-          type="submit"
-          variant="contained"
-          color="primary"
-          style={{ marginTop: "10px" }}
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity={snackbarSeverity}
+          sx={{ width: "100%" }}
         >
-          Update
-        </Button>
-      </form>
-    </div>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Dialog>
   );
 };
 
